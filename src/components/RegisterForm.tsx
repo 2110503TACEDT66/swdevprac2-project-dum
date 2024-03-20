bc8e3e2cc078e08fab0bf4acb54f1a6e2816ecec
TenJit <tenjitrattanakul@gmail.com>
2024-03-21 01:30:26 +0700

    Login and registerform

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -4,50 +4,50 @@ import styles from './registerform.module.css'
 export default function RegisterForm(){
     return (
         <div className={styles.registerBlock}>
+            <h1 className={styles.title}>Register</h1>
             <form id='register-form' className={styles.registerForm}>
                 <TextField
                 required
                 id="name"
                 label="Name-Lastname"
-                variant="filled"
-                sx={{ width: '85%' }}
+                variant="outlined"
                 className={styles.TextField}
                 margin='dense'/>
                 <TextField
                 required
                 id="email"
                 label="Email"
-                variant="filled"
-                sx={{ width: '85%' }}
+                variant="outlined"
+                
                 className={styles.TextField}
                 margin='dense'/>
                 <TextField
                 required
                 id="telnumber"
                 label="Telephone number"
-                variant="filled"
-                sx={{ width: '85%' }}
+                variant="outlined"
+                
                 className={styles.TextField}
                 margin='dense'/>
                 <TextField
                 required
                 id="password"
                 label="Create your password"
-                variant="filled"
-                sx={{ width: '85%' }}
+                variant="outlined"
+                
                 type='password'
                 className={styles.TextField}
                 margin='dense'/>
                 <TextField
                 required
-                id="password"
+                id="confirm-password"
                 label="Confirm your password"
-                variant="filled"
-                sx={{ width: '85%' }}
+                variant="outlined"
+                
                 type='password'
                 className={styles.TextField}
                 margin='dense'/>
-                <button>
+                <button className={styles.registerButton} type='submit'>
                     Register
                 </button>
             </form>
