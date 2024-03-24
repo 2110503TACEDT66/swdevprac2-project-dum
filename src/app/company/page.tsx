6135ca8cfc292ac010caa5e4b06da6747c27048c
TenJit <tenjitrattanakul@gmail.com>
2024-03-25 03:12:31 +0700

    Company panel with mock info

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -4,9 +4,15 @@ import CompanyIntBlock from '@/components/CompanyIntBlock/CompanyBlock'
 import CompanyIntPanel from '@/components/CompanyIntPanel/CompanyIntPanel'
 import TimeSlot from '@/components/TimeSlot/TimeSlot'
 import TimeSlotPanel from '@/components/TimeSlotPanel/TimeSlotPanel'
+import CompanyBlock from '@/components/CompanyBlock/CompanyBlock'
+import CompanyPanel from '@/components/CompanyPanel/CompanyPanel'
 
 export default function CompanyPage() {
     return (
-        <TimeSlotPanel/>
+        <div>
+            {/* <TimeSlotPanel/> */}
+            <CompanyPanel/>
+        </div>
+        
     )
 }
--- src/components/TimeSlotPanel/TimeSlotPanel.tsx
+++ src/components/TimeSlotPanel/TimeSlotPanel.tsx
@@ -17,12 +17,12 @@ export default function TimeSlotPanel(){
         },
         {id: "002",compName: "Aunnoon",image: "/Icon/account-black.png",currentCapa: 2,maxCapa: 5,date: new Date('2023-03-25'),time: '10:30',reserv: false,
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
-        {id: "003",compName: "Aunneeeee",image: "/Icon/account-black.png",currentCapa: 1,maxCapa: 4,date: new Date('2023-03-26'),time: '16:00',reserv: true,
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
-        {id: "004",compName: "Aunni",image: "/Icon/account-black.png",currentCapa: 3,maxCapa: 3,date: new Date('2023-03-27'),time: '09:00',reserv: false,
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
-        {id: "005",compName: "Aunnnn",image: "/Icon/account-black.png",currentCapa: 0,maxCapa: 2,date: new Date('2023-03-28'),time: '13:30',reserv: true,
-        desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
+        // {id: "003",compName: "Aunneeeee",image: "/Icon/account-black.png",currentCapa: 1,maxCapa: 4,date: new Date('2023-03-26'),time: '16:00',reserv: true,
+        // desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
+        // {id: "004",compName: "Aunni",image: "/Icon/account-black.png",currentCapa: 3,maxCapa: 3,date: new Date('2023-03-27'),time: '09:00',reserv: false,
+        // desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
+        // {id: "005",compName: "Aunnnn",image: "/Icon/account-black.png",currentCapa: 0,maxCapa: 2,date: new Date('2023-03-28'),time: '13:30',reserv: true,
+        // desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
         // {id: "006",compName: "Aunnannnnnn",image: "/Icon/account-black.png",currentCapa: 0,maxCapa: 2,date: new Date('2023-03-28'),time: '13:30',reserv: true,
         // desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,...."},
         // {id: "007",compName: "Aunnannnnnn",image: "/Icon/account-black.png",currentCapa: 0,maxCapa: 2,date: new Date('2023-03-28'),time: '13:30',reserv: true,
@@ -36,7 +36,6 @@ export default function TimeSlotPanel(){
       ];
 
     return(
-        <div className={styles.mock}>
             <div className={styles.fullBlock}>
                 {mockTimeslotRepo.map((timeslot) => (
                         <TimeSlot currentCapacity={timeslot.currentCapa}
@@ -47,8 +46,6 @@ export default function TimeSlotPanel(){
                             desc={timeslot.desc}/>
                 ))}
             </div>
-        </div>
-        
     )
     
 }
