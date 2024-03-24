1645aabb810c777cf60e4dfe707d9ea6091386bf
NATCH <nat.siriruangbun@gmail.com>
2024-03-24 22:36:49 +0700

    I dont know What I'm Doing

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,9 @@ import styles from './page.module.css'
 export default function ProfilePage () {
     return (
        <section className={styles.Sec1}>
-            <div className={styles.DetailContainer}></div>
+            <div className={styles.DetailContainer}>
+                <div className={styles.ProfilePicWrapper}></div>
+            </div>
        </section>
     )
 }
