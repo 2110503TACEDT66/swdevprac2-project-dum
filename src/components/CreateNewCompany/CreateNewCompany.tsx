c8523009f28c546448e78120b6e761474ed8d5be
TenJit <tenjitrattanakul@gmail.com>
2024-03-26 13:25:02 +0700

    CreateNewCompany

--- src/components/CreateNewCompany/CreateNewCompany.tsx
+++ src/components/CreateNewCompany/CreateNewCompany.tsx
@@ -1,5 +1,8 @@
 import styles from './createnewcompany.module.css'
 import { Button, TextField } from '@mui/material';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { IconButton } from '@mui/material';
+import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
 
 export default function CreateNewCompany() {
     
@@ -7,66 +10,78 @@ export default function CreateNewCompany() {
         <div className={styles.fullBlock}>
             <div className={styles.topPart}>
                 <div className={styles.imageBlock}>
-                    <img src='/Icon/account-black.png' alt='Profile picture'/>
+                    <img src='/Icon/account-black.png' alt='Profile picture' className={styles.image}/>
+                    <div className={styles.iconBlock}>
+                    <IconButton aria-label="edit">
+                        <PhotoCameraIcon className={styles.cameraButton}/>
+                    </IconButton> 
+                    </div>
                 </div>
                 <div className={styles.textBlock}>
                     <div className={styles.rowBlock}>
                         <div className={styles.topicBlock}>
-
+                            Name
                         </div>
                         <div className={styles.inputBlock}>
-
+                            <TextField id="name" variant="standard" className={styles.textField}/>
                         </div>
                     </div>
                     <div className={styles.rowBlock}>
                         <div className={styles.topicBlock}>
-
+                            Website
                         </div>
                         <div className={styles.inputBlock}>
-
+                            <TextField id="website" variant="standard" className={styles.textField}/>
                         </div>
                     </div>
                     <div className={styles.rowBlock}>
-                        <div>
-
+                        <div className={styles.topicBlock}>
+                            Tel. number
                         </div>
-                        <div>
-
-                        </div>
-                    </div>
-                    <div className={styles.rowBlock}>
-                        <div>
-
-                        </div>
-                        <div>
-
+                        <div className={styles.inputBlock}>
+                            <TextField id="tel" variant="standard" className={styles.textField}/>
                         </div>
                     </div>
                     <div className={styles.rowBlock}>
-                        <div>
-
+                        <div className={styles.topicBlock}>
+                            Sign-up Email
                         </div>
-                        <div>
-
-                        </div>
-                    </div>
-                    <div className={styles.rowBlock}>
-                        <div>
-
-                        </div>
-                        <div>
-
+                        <div className={styles.inputBlock}>
+                            <TextField id="signupemail" variant="standard" className={styles.textField}/>
                         </div>
                     </div>
                     <div className={styles.rowBlock}>
-                        <div>
-                            
+                        <div className={styles.topicBlock}>
+                            Contact Email
+                        </div>
+                        <div className={styles.inputBlock}>
+                            <TextField id="contactemail" variant="standard" className={styles.textField}/>
+                        </div>
+                    </div>
+                    <div className={styles.rowBlock}>
+                        <div className={styles.topicBlock}>
+                            Description
+                        </div>
+                        <div className={styles.inputBlock}>
+                            <TextField id="desc" variant="standard" className={styles.textField}/>
+                        </div>
+                    </div>
+                    <div className={styles.rowBlock}>
+                        <div className={styles.topicBlock}>
+                            Password
+                        </div>
+                        <div className={styles.inputBlock}>
+                            <TextField id="password" type="password" variant="standard" className={styles.textField}/>
                         </div>
                     </div>
                 </div>
             </div>
             <div className={styles.botPart}>
-
+                <div className={styles.buttonBlock}>
+                    <Button variant="contained" className={styles.addButton}>
+                        Create new Company
+                    </Button>
+                </div>
             </div>
         </div>
     )
