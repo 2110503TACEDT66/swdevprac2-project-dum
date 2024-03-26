9174143525be653d5dea7d15dfdcb96915afb7ac
TenJit <tenjitrattanakul@gmail.com>
2024-03-26 15:17:22 +0700

    Admin Profile

---
+++ src/app/a/testprofile/page.tsx
@@ -0,0 +1,9 @@
+import AdminProfile from "@/components/AdminProfile/AdminProfile";
+
+export default function TestProfile(){
+    return(
+        <div>
+            <AdminProfile/>
+        </div>
+    )
+}
